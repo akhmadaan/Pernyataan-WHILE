460324935cb57e4b0401fa91303a5e9b053683db
AKHMAD SAIFUL ANAM <45529658+akhmadaan@users.noreply.github.com>
2019-01-06 01:51:52 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<stdlib.h>
+
+
+using namespace std;
+
+int main()
+{
+    system("color a");
+    int a=1;
+     while(a<=10)
+
+    {
+
+        cout<<"nomor"<<a<<endl;
+        ++a;
+    }
+
+    return 0;
+}
